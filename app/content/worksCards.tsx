eaf25b57371ea0117dc673479459fc54c870ac87
mikey <mikeygeraghty@gmail.com>
2024-04-29 17:20:31 +0100

    json

--- app/content/worksCards.tsx
+++ app/content/worksCards.tsx
@@ -1,4 +1,4 @@
-import worksData from "./data.json"; // Import the default export from data.json
+import worksData from "./data.json";
 
 import { useState, useEffect } from "react";
 import {
@@ -8,29 +8,35 @@ import {
   useDisclosure,
   Link,
   Button,
-} from "@nextui-org/react"; // import your Card, Image, Chip components from your library
+} from "@nextui-org/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExpand } from "@fortawesome/free-solid-svg-icons";
+import {
+  faExpand,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
 
 import NextImage from "next/image";
 
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-} from "@nextui-org/modal";
+import { Modal, ModalContent, ModalBody, ModalFooter } from "@nextui-org/modal";
 
 // Define interface for work data
 type Work = {
   company_name: string;
-  img: {
-    src: string;
+  card: {
+    img: {
+      src: string;
+      height: number;
+      width: number;
+    };
+    built_with: string[];
+  };
+  modal: {
+    img: string;
+    desc: string;
     height: number;
     width: number;
-  };
-  built_with: string[];
+    alt: string;
+  }[];
 };
 
 const WorksCard = () => {
@@ -62,13 +68,13 @@ const WorksCard = () => {
             </div>
             <Image
               as={NextImage}
-              src={work.img.src}
-              height={work.img.height}
-              width={work.img.width}
+              src={work.card.img.src}
+              height={work.card.img.height}
+              width={work.card.img.width}
               alt={`Desktop screenshot of ${work.company_name}`}
             />
             <div className="flex items-center justify-between">
-              {work.built_with.map((tech, index) => (
+              {work.card.built_with.map((tech, index) => (
                 <Chip key={index} color="default" radius="md">
                   {tech}
                 </Chip>
@@ -89,58 +95,25 @@ const WorksCard = () => {
                 <>
                   <ModalBody className="gap-8">
                     <header className="flex flex-initial py-4 text-[2rem] font-semibold">
-                      BGRSolutions
+                      {work.company_name}
                     </header>
-                    <p className="text-zinc-300">
-                      A dynamic e-commerce solution using WordPress and
-                      WooCommerce. Seamlessly integrating CSV imports and
-                      translation plugins to ensure smooth operations.
-                    </p>
-                    <div className="flex w-full justify-center rounded-[1rem] bg-zinc-800 py-2">
-                      <Image
-                        as={NextImage}
-                        width={200}
-                        height={300}
-                        src="/bgrHome-mobile.webp"
-                        alt="A mobile screenshot of www.bgrsolutions.net homepage"
-                      ></Image>
-                    </div>
-                    <p className="text-zinc-300">
-                      With a knack for customization, I have added bespoke
-                      HTML,CSS and JavaScript, elements when necessary,
-                      enhancing functionality and aesthetics.
-                    </p>
-                    <div className="flex w-full justify-center rounded-[1rem] bg-zinc-800 p-2">
-                      <Image
-                        as={NextImage}
-                        width={400}
-                        height={300}
-                        src="/bgrSamsung.webp"
-                        alt="A mobile screenshot of www.bgrsolutions.net homepage"
-                      ></Image>
-                    </div>
-
-                    <p className="text-zinc-300">
-                      Designing promotional and in-store graphics. From
-                      eye-catching posters to captivating digital displays, I
-                      excel in crafting visually appealing assets that elevate
-                      brand presence and drive engagement both online and
-                      offline.
-                    </p>
-                    <Image
-                      as={NextImage}
-                      width={400}
-                      height={300}
-                      src="/bgr-media.webp"
-                      alt="A graphic of a multimedia banner for an ecommerce"
-                    ></Image>
-                    <Image
-                      as={NextImage}
-                      width={400}
-                      height={300}
-                      src="/bgr-phones.webp"
-                      alt="A graphic of a smartphone banner for an ecommerce"
-                    ></Image>
+                    {work.modal.map((modal, index) => (
+                      <>
+                        <div
+                          className="flex w-full justify-center rounded-[1rem] bg-zinc-800 py-2"
+                          key={index}
+                        >
+                          <Image
+                            as={NextImage}
+                            width={modal.width}
+                            height={modal.height}
+                            src={modal.img}
+                            alt={modal.alt}
+                          ></Image>
+                        </div>
+                        <p className="text-zinc-300">{modal.desc}</p>
+                      </>
+                    ))}
                   </ModalBody>
                   <ModalFooter className="">
                     <Button fullWidth={true} color="primary">
@@ -165,5 +138,4 @@ const WorksCard = () => {
     </div>
   );
 };
-
 export default WorksCard;
