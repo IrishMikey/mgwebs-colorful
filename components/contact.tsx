781b306754ab048785393fcd95bae76085965ee8
IrishMikey <mikeygeraghty@gmail.com>
2024-07-31 20:46:15 +0100

    fixed photo width & coming soon form

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,7 +6,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
-
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
 
@@ -14,7 +13,7 @@ export default function Contact() {
     <motion.section
       ref={ref}
       id="contact"
-      className="mb-28 flex w-full scroll-mt-[7rem] flex-col leading-8 sm:mb-40"
+      className="mb-28 flex w-full scroll-mt-[7rem] flex-col leading-8 sm:mb-40 relative"
       initial={{
         opacity: 0,
       }}
@@ -29,48 +28,55 @@ export default function Contact() {
       <span className="mb-3 text-zinc-300 sm:max-w-[75ch] sm:text-xl">
         Connect with me for collaborations, questions, or just to say hello!
       </span>
-      <span className=" mb-3 gap-2 sm:text-xl flex">
+      <span className=" mb-3 flex gap-2 sm:text-xl">
         <Link
-          className="text-md text-zinc-300 bg-zinc-800 rounded-xl"
+          className="text-md rounded-xl bg-zinc-800 text-zinc-300"
           isBlock
           href="mailto:mikeygeraghty@gmail.com"
         >
           Email
         </Link>
         <Link
-          className="text-md text-zinc-300 bg-zinc-800 rounded-xl"
+          className="text-md rounded-xl bg-zinc-800 text-zinc-300"
           isBlock
           href="https://www.linkedin.com/in/michaeldanielgeraghty/"
         >
           LinkedIn
         </Link>
         <Link
-          className="text-md text-zinc-300  bg-zinc-800 rounded-xl"
+          className="text-md rounded-xl  bg-zinc-800 text-zinc-300"
           isBlock
           href="https://twitter.com/MGWebss"
         >
           X
         </Link>
       </span>
-      <form className="mt-10">
-        <Input
-          type="email"
-          label="Email"
-          isDisabled
-          defaultValue="Coming soon"
-          className="h-14"
-        />
-        <Textarea
-          label="Message"
-          placeholder=""
-          isDisabled
-          className="h-50 my-3 mt-5"
-        ></Textarea>
-        <Button color="default" isDisabled className=" text-lg font-bold" size="lg">
-          Send
-          <FontAwesomeIcon icon={faChevronRight} />
-        </Button>
-      </form>
+      <div className="bg-[#222222] flex justify-center items-center z-10  bg-opacity-75 shadow-lg w-full shadow-black/[0.03] backdrop-blur-[0.05rem] h-[276px] absolute bottom-[-20px] rounded-lg">
+      <span className="">Coming soon</span></div>
+        <form className="mt-10 mx-4">
+          <Input
+            type="email"
+            label="Email"
+            isDisabled
+            className="h-14"
+          />
+          <Textarea
+            label="Message"
+            placeholder=""
+            isDisabled
+            className="h-50 my-3 mt-5"
+          ></Textarea>
+          <Button
+            color="default"
+            isDisabled
+            className=" text-lg font-bold"
+            size="lg"
+          >
+            Send
+            <FontAwesomeIcon icon={faChevronRight} />
+          </Button>
+        </form>
+      
     </motion.section>
   );
 }
--- components/photo.tsx
+++ components/photo.tsx
@@ -7,7 +7,6 @@ import Image from "next/image";
 import { Card, CardBody } from "@nextui-org/react";
 
 export default function Photo({ imageUrl }: GalleryProps) {
-  const photoWidth = imageUrl.width
   return (
     <Card className="">
       <CardBody className=" ">
@@ -15,7 +14,7 @@ export default function Photo({ imageUrl }: GalleryProps) {
         src={imageUrl}
         alt=""
         quality={95}
-        className="object-cover rounded-xl max-w-[364px] sm:max-w-[300px]"
+        className="object-cover rounded-xl  sm:max-w-[300px]"
       ></Image>
       </CardBody>
     </Card>
