964f6135eb2645a46cb4130cf01468423a8d1cd9
Mikey G <mikeygeraghty@gmail.com>
2024-08-02 17:51:25 +0200

    footer responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
     <footer className="m-auto flex max-w-[50rem] flex-col items-center px-4">
       <div className="flex w-full rounded-t-lg bg-zinc-800 shadow min-h-20 px-4">
 
-      <ul className="flex justify-around items-center flex-grow flex-col">
-        <li>MGWebs 2024</li>
+      <ul className="flex justify-around items-center flex-grow flex-col sm:flex-row ">
+        <li className="text-zinc-400">MGWebs 2024</li>
         <li className="text-zinc-400">This webpage is a work-in-progress</li>
       </ul>
       </div>
