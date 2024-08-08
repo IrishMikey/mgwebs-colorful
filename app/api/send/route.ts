279216cd04f88bc572c9a3cfcc9e9e35c10444fb
mikey <mikeygeraghty@gmail.com>
2024-08-08 10:04:47 +0100

    init - docs copied - not working

---
+++ app/api/send/route.ts
@@ -0,0 +1,26 @@
+
+import { EmailTemplate } from '@/components/email-template';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+"use server"
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Mikey <contact@mgwebs.com>',
+      to: ['mikeygeraghty@gmail.com'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
