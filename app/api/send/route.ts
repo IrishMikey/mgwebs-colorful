f9c330a99640214052f57ba8267d629b6f8cd1bf
mikey <mikeygeraghty@gmail.com>
2024-08-09 16:41:51 +0100

    update to api routes for vercel

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -4,8 +4,29 @@ import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/components/email-template";
+import { NextResponse } from "next/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+
+
+export async function POST(request: Request) {
+  // Extract form data from the request
+  const formData = await request.formData();
+
+  // You can now access the form fields using formData.get("fieldName")
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  // Pass the formData object to your sendEmail function
+  const result = await sendEmail(formData);
+
+  if (result.error) {
+    return Response.json({ error: result.error }, { status: 400 });
+  }
+
+  return NextResponse.json({ success: true, data: result.data });
+}
+
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
