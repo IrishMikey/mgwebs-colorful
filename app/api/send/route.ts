bd7b595f5441024d03cc7d044b678b028675f711
mikey <mikeygeraghty@gmail.com>
2024-08-09 12:19:58 +0100

    working test with update api

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -6,8 +6,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function GET() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Mikey <contact@mgwebs.com>',
-      to: ['contact@mgwebs.com'],
+      from: 'email@mgwebs.com',
+      to: ['mikeygeraghty@gmail.com'],
       subject: 'Hello world',
       react: EmailTemplate({ firstName: 'John' }),
     });
@@ -16,7 +16,7 @@ export async function GET() {
       return Response.json({ error }, { status: 500 });
     }
 
-    return Response.json(data);
+    return Response.json({status: "ok"});
   } catch (error) {
     return Response.json({ error }, { status: 500 });
   }
